7a70c82e28bbbe711679e3b66626ce6981609a19
Jon Newton <jon.newton@gmail.com>
2014-12-31 07:57:21 -0700

    Algebraic types for AST

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+
+// AST //////////////////////////////
+
+enum Keyword {
+    If,
+    Define,
+    Quote,
+    Lambda,
+    Set
+}
+
+enum Symbol {
+    Keyword,
+    NonKeyword(str)
+}
+
+enum Atom {
+    Number,
+    Symbol
+}
+
+enum SymbolicExpr {
+    Atom,
+    ListExpr(Vec<SymbolicExpr>)
+}
+
 fn main() {
+    let x = 5i;
+
     println!("Hello, world!");
+    println!("{}", x);
 }
